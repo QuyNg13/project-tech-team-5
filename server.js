cad3b73ed3c2067ab188c1b66fd18f592ba4733f
fouzialhmdi <112877092+fouzialhmdi@users.noreply.github.com>
2024-03-08 09:53:12 +0100

    fix registratie

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const { MongoClient } = require("mongodb")
 const app = express()
 const port = 3000
+
 require('dotenv').config()
 
-const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_DB;
 const client = new MongoClient(uri);
 
@@ -39,6 +40,25 @@ app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static('style'))
 
+//Endpoint om gebruikers op te halen
+app.get('/users', async (req, res) => {
+  try {
+    await client.connect()
+    const db = client.db("Data")
+    const coll = db.collection("users")
+    const result = await coll.find({}).toArray()
+    res.json(result)
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({error: 'Er is een fout opgetreden bij het ophalen van gebruikers'})
+  } finally {
+    await client.close ()
+  }
+
+})
+
+
 app.get('/', (req, res) => {
   res.render('base')
 })
@@ -47,7 +67,11 @@ app.get('/register', (req, res) => {
   res.render('register')
 })
 
-app.post('/', adduser)
+// app.post('/', adduser)
+
+app.post('/', async(req,res) => {
+  await adduser(req, res)
+})
 
 async function adduser(req,res) {
   try {
@@ -55,16 +79,39 @@ async function adduser(req,res) {
     const { username, password } = req.body
     const db = client.db("Data")
     const coll = db.collection("users")
-    const { ObjectId } = require("mongodb")
+    const {insertId} = require("mongodb")
     const result = await coll.insertOne({ username, password })
     console.log(result.insertedId)
-  } finally {
-    await client.close();
+    res.send('Gebruiker toegevoegd')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Er is een fout opgetreden bij het toevoegen van de gebruiker')
+  }
+  finally {
+    await client.close ()
   }
 }
-adduser().catch(console.dir);
+ 
+
+// adduser().catch(console.dir);
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+// mongodb client openen wanneer de applicatie start 
+
+client.connect().then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
+})
+
+//afsluiten van mongodb client wanneer de applicatie wordt gesloten
+process.on ('SIGINT', () => {
+  client.close().then(() => {
+    console.log('MongoDB client gesloten.')
+    process.exit(0)
+  })
 })
