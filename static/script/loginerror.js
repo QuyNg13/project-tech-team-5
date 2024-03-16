ac0de7a40f8587bbd10247d4d7eb4b398cc6cdb3
Quy Nguyen <quythieu34@gmail.com>
2024-03-16 11:57:25 +0100

    Getqueryvariable aangepast

--- static/script/loginerror.js
+++ static/script/loginerror.js
@@ -1,14 +1,7 @@
         // Functie om queryparameters uit de URL te halen
         function getQueryVariable(variable) {
-            const query = window.location.search.substring(1);
-            const vars = query.split('&');
-            for (let i = 0; i < vars.length; i++) {
-                const pair = vars[i].split('=');
-                if (decodeURIComponent(pair[0]) === variable) {
-                    return decodeURIComponent(pair[1]);
-                }
-            }
-            return null;
+            const params = new URLSearchParams(window.location.search);
+            return params.get(variable);
         }
 
         // Haal de waarde van de 'error' queryparameter op
