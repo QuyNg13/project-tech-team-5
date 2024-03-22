9d13b4716da317353fda7fd5a3a34ab3bf89be94
desmandemaker <destiny.mandemaker@hva.nl>
2024-03-22 14:35:55 +0100

    om te kijken of dat klopt

--- static/script/script.js
+++ static/script/script.js
@@ -49,7 +49,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-/* vanaf hier js voor dropdown*/
+/* vanaf hier js voor dropdown oude data van de opdracht vervangen voor de data van ons zelf!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function toggleDropdown() {
@@ -65,80 +69,93 @@ function toggleDropdown() {
 };
 
 
-const charactersList = new List('theList', options);
-
-//Dit is voor de buttons van sorteren op categorie
+// //Dit is voor de buttons van sorteren op categorie
 
 
-// Stap 1: Selecteer het element met het id "filter-all" en wijs het toe aan de constante optionAll
-const optionAll = document.querySelector("#filter-all");
+// // Stap 1: Selecteer het element met het id "filter-all" en wijs het toe aan de constante optionAll
+// const optionAll = document.querySelector("#filter-all");
 
-// Stap 2: Selecteer het element met het id "filter-people" en wijs het toe aan de constante optionPeople
-const optionPeople = document.querySelector("#filter-people");
+// // Stap 2: Selecteer het element met het id "filter-people" en wijs het toe aan de constante optionPeople
+// const optionPeople = document.querySelector("#filter-people");
 
-// Stap 3: Selecteer het element met het id "filter-animals" en wijs het toe aan de constante optionAnimals
-const optionAnimals = document.querySelector("#filter-animals");
+// // Stap 3: Selecteer het element met het id "filter-animals" en wijs het toe aan de constante optionAnimals
+// const optionAnimals = document.querySelector("#filter-animals");
 
-// Stap 4: Selecteer het element met het id "filter-items" en wijs het toe aan de constante optionItems
-const optionItems = document.querySelector("#filter-items");
-
-
-// Stap 2.
-optionAll.addEventListener("change", filterList);
-optionPeople.addEventListener("change", filterList);
-optionAnimals.addEventListener("change", filterList);
-optionItems.addEventListener("change", filterList);
+// // Stap 4: Selecteer het element met het id "filter-items" en wijs het toe aan de constante optionItems
+// const optionItems = document.querySelector("#filter-items");
 
 
-// Stap 3.
-function filterList(event){
-  /* de ul opzoeken */
-  let deLijst = document.querySelector("ul");
-  let nieuweFilter = event.target.value;
-  // verwijder de huidige class van de lijst
-  deLijst.className = "";
-  // voeg de nieuwe class toe aan de lijst
-  // het event object bevat alle info van het event dat heeft plaatsgevonden
-  // het target is het element dat het event heeft laten afgaan
-  // in dit geval de radio button die gewijzigd is
-  // de value van die radio button is de nieuwe class
-  deLijst.classList.add(nieuweFilter);
+// // Stap 2.
+// optionAll.addEventListener("change", filterList);
+// optionPeople.addEventListener("change", filterList);
+// optionAnimals.addEventListener("change", filterList);
+// optionItems.addEventListener("change", filterList);
+
+
+// // Stap 3.
+// function filterList(event){
+//   /* de ul opzoeken */
+//   let deLijst = document.querySelector("ul");
+//   let nieuweFilter = event.target.value;
+//   // verwijder de huidige class van de lijst
+//   deLijst.className = "";
+//   // voeg de nieuwe class toe aan de lijst
+//   // het event object bevat alle info van het event dat heeft plaatsgevonden
+//   // het target is het element dat het event heeft laten afgaan
+//   // in dit geval de radio button die gewijzigd is
+//   // de value van die radio button is de nieuwe class
+//   deLijst.classList.add(nieuweFilter);
+// }
+
+// // Stap 4.
+// // Verder met de CSS
+
+// //dit is voor tonen in grid of lijst
+
+
+// // stap 1.
+// var optionList = document.querySelector("#view-list");
+// var optionGrid = document.querySelector("#view-grid");
+
+
+// // stap 2 en 3.
+// optionList.addEventListener("change", showInList);
+// optionGrid.addEventListener("change", showInGrid);
+
+
+// // stap 4.
+// function showInList() {
+//   /* de lijst in de HTML opzoeken */
+//   let deLijst = document.querySelector("ul");
+//   /* de class grid-view verwijderen */
+//   deLijst.classList.remove("grid-view");
+//   /* dan de class list-view toevoegen */
+//   deLijst.classList.add("list-view");
+// }
+
+
+// // stap 5.
+// function showInGrid() {
+//   /* de lijst in de HTML opzoeken */
+//   let deLijst = document.querySelector("ul");
+//   /* de class list-view verwijderen */
+//   deLijst.classList.remove("list-view");
+//   /* dan de class grid-view toevoegen */
+//   deLijst.classList.add("grid-view");
+// }
+
+// // stap 6.
+// // in de CSS
+
+function filterGames() {
+  const selectedGenres = Array.from(document.getElementById('genres').selectedOptions).map(option => option.value);
+  console.log(selectedGenres); // Dit logt een array van de geselecteerde genres in de console
+  // Voer hier je logica uit om de games te filteren op basis van de geselecteerde genres
 }
 
-// Stap 4.
-// Verder met de CSS
-
-// stap 1.
-var optionList = document.querySelector("#view-list");
-var optionGrid = document.querySelector("#view-grid");
-
-
-// stap 2 en 3.
-optionList.addEventListener("change", showInList);
-optionGrid.addEventListener("change", showInGrid);
-
-
-// stap 4.
-function showInList() {
-  /* de lijst in de HTML opzoeken */
-  let deLijst = document.querySelector("ul");
-  /* de class grid-view verwijderen */
-  deLijst.classList.remove("grid-view");
-  /* dan de class list-view toevoegen */
-  deLijst.classList.add("list-view");
-}
-
-
-// stap 5.
-function showInGrid() {
-  /* de lijst in de HTML opzoeken */
-  let deLijst = document.querySelector("ul");
-  /* de class list-view verwijderen */
-  deLijst.classList.remove("list-view");
-  /* dan de class grid-view toevoegen */
-  deLijst.classList.add("grid-view");
-}
-
-// stap 6.
-// in de CSS
-  
+fetch('https://api.rawg.io/api/platforms')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => console.error('Error fetching platforms:', error));
