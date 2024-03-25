b2516ab091aeee843a7bff340e30085785f56110
DBKoekkoek <151542447+DBKoekkoek@users.noreply.github.com>
2024-03-25 17:57:39 +0100

    InstellingenProfiel CSS, HTML & JAvascript start
    HTML en Css heeft nu een start van, alles staat netjes erin. Ik ga verder met het onderzoeken van hoe ik data uiteindelijk ik erin kan zetten.... Na de gym :)

--- server.js
+++ server.js
@@ -196,3 +196,26 @@ app.post('/addfriend/:friendId'), async (req, res) => {
 
 
 //Backend Daan Kkoekkoek Eigenschappen
+const userSchema = new mongoose.Schema({
+  username: String,
+  gender: String,
+  age: Number,
+  language: String,
+  bio: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.get('/user', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
