2a5bc491a9f492ff620486906d2092ee2e04ddd1
fouzialhmdi <112877092+fouzialhmdi@users.noreply.github.com>
2024-04-02 16:10:26 +0200

    laatste wijzigingen in de les

--- server.js
+++ server.js
@@ -265,14 +265,9 @@ app.post('/addfriend/:friendId', async (req, res) => {
     const friendId = req.params.friendId
 
     await coll.updateOne(
-      {_id: new ObjectId(friendId)},
-      { $addToSet: {friendshipRequests: new ObjectId(req.session.user._id) } }
+      {_id: new ObjectId(req.session.user._id)},
+      { $addToSet: {friends: new ObjectId(friendId) } }
     )
-
-    // await coll.updateOne(
-    //   {_id: new ObjectId(req.session.user._id)},
-    //   { $addToSet: {friends: new ObjectId(friendId) } }
-    // )
 
     // Swal.fire({
     //   title: "Confirmation",
@@ -295,8 +290,17 @@ app.get('/friendrequests', checkLoggedIn,  async (req, res) => {
       console.error('User session is not set or missing user ID')
       return res.status(401).json({ error: 'Unauthorized' })
     }
-  
-    const friendId = req.params.friendI
+    //ID van de ontvanger ophalen
+    const receiverId = req.session.user._id
+
+    await client.connect ()
+    const db = client.db("Data")
+    const coll = db.collection("users")
+    const user = await coll.findOne({_id: new ObjectId(receiverId)})
+
+    const friendshipRequests = user.friendshipRequests || []
+
+    res.render('vriendschapsverzoeken', {friendshipRequests, user: req.session.user})
 
   } catch (error) {
     console.error('Error fetching friendship requests:', error)
@@ -322,11 +326,11 @@ app.post('/accept-friend-request/friendId', checkLoggedIn, async (req, res) => {
       return res.status(404).json({ error: 'Friendship request was not found'})
     }
 
-    // Swal.fire({
-    //   title: "Confirmation",
-    //   text: "Friendship request accepted",
-    //   icon: "success"
-    // })
+    Swal.fire({
+      title: "Confirmation",
+      text: "Friendship request accepted",
+      icon: "success"
+    })
 
     res.status(200).json({message: 'Friendschip request succesfully accepted'})
   } catch (error) {
